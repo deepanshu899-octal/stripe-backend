d4d3b05a22e2a303e20ac8bafd5506db83a4340f
deepanshu.gupta <deepanshu.gupta@octalsoftware.com>
2024-10-11 16:58:02 +0530

    add paypal

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const Stripe = require('stripe');
 const bodyParser = require('body-parser');
+
+// const paypal = require('paypal-rest-sdk');
+const paypal = require('@paypal/checkout-server-sdk');
 const cors = require('cors');
 
 const app = express();
@@ -13,6 +16,18 @@ const stripe = Stripe('sk_test_51Q8HzUJru0Ak1tZZP85cmvx6FsQwGFtZVqjmxrux5pC4sY7C
 // Middleware
 app.use(cors()); // Enable CORS
 app.use(bodyParser.json()); // Parse JSON bodies
+
+// paypal.configure({
+//     mode: 'sandbox', // Use 'sandbox' for testing and 'live' for production
+//     client_id: 'AXxWPKqsl8E9c24UfEoVNQGZQ9BuBSqk8Dwi0kwmMtdULaDXcKd7CWUy5SVhj6VHqJOwHUXTD1q4bpyj',
+//     client_secret: 'ENd4lmCtYa2Dn-N6Rw7X_L2k9HoeDVrwyFId9fbaEAk5uDlrU5E9crri_OTI2-5A9nnnQZjCn6R5X6Pp'
+// });
+
+const clientId = 'AXxWPKqsl8E9c24UfEoVNQGZQ9BuBSqk8Dwi0kwmMtdULaDXcKd7CWUy5SVhj6VHqJOwHUXTD1q4bpyj';
+const clientSecret = 'ENd4lmCtYa2Dn-N6Rw7X_L2k9HoeDVrwyFId9fbaEAk5uDlrU5E9crri_OTI2-5A9nnnQZjCn6R5X6Pp';
+
+const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
+const client = new paypal.core.PayPalHttpClient(environment);
 
 // Route to create a payment intent
 app.post('/api/stripe/create-payment-intent', async (req, res) => {
@@ -35,8 +50,8 @@ app.post('/api/stripe/create-payment-intent', async (req, res) => {
 
         res.status(200).send({ client_secret: paymentIntent.client_secret, paymentId: paymentIntent.id });
     } catch (error) {
-        res.status(500).send({ error: error.message });
         console.log(error.message)
+        res.status(500).send({ error: error.message });
     }
 });
 
@@ -87,6 +102,54 @@ app.get('/api/stripe/check-payment-status/:paymentIntentId', async (req, res) =>
       res.status(500).send({ error: error.message });
     }
   });
+
+
+// Set up PayPal environment
+app.post('/api/paypal/create-payment', async (req, res) => {
+    const { amount } = req.body;
+console.log('paylapl...')
+    const request = new paypal.orders.OrdersCreateRequest();
+    request.prefer("return=representation");
+    request.requestBody({
+        intent: "CAPTURE",
+        purchase_units: [{
+            amount: {
+                currency_code: "USD",
+                value: 11
+            }
+        }]
+    });
+
+    try {
+        const order = await client.execute(request);
+        res.json(order.result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to create payment' });
+    }
+});
+
+
+  //bank transfer
+app.post('/banktransfer/create-payment-intent', async (req, res) => {
+    try {
+        const { amount } = req.body; // Amount in cents
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: 'usd', // Change currency as needed
+            payment_method_types: ['us_bank_account'], // Specify bank account as payment method
+        });
+        console.log(`Payment intent created for , id - ${paymentIntent.id}  with amount: ${amount}`);
+        res.send({ clientSecret: paymentIntent.client_secret });
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({ error: error.message });
+    }
+});
+
+
+
+
 
 // Start the server
 app.listen(PORT, () => {
